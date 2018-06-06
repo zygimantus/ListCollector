8afe62d9040e72ca612516bd22302552efed1e05
Zygimantus <zygimantus@gmail.com>
2018-06-06 17:33:21 +0300

    Spring Boot used

--- src/main/java/com/zygimantus/listcollector/HelloController.java
+++ src/main/java/com/zygimantus/listcollector/HelloController.java
@@ -6,7 +6,9 @@ import javafx.scene.control.TextField;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class HelloController
 {
     private static final Logger log = LoggerFactory.getLogger(HelloController.class);
--- src/main/java/com/zygimantus/listcollector/MainApp.java
+++ src/main/java/com/zygimantus/listcollector/MainApp.java
@@ -7,23 +7,36 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@SpringBootApplication
 public class MainApp extends Application {
 
     private static final Logger log = LoggerFactory.getLogger(MainApp.class);
 
+    private ConfigurableApplicationContext springContext;
+    private Parent rootNode;
+
     public static void main(String[] args) throws Exception {
-        launch(args);
+        Application.launch(args);
     }
 
+    @Override
+    public void init() throws Exception {
+        springContext = SpringApplication.run(MainApp.class);
+        String fxmlFile = "/fxml/hello.fxml";
+        log.debug("Loading FXML for main view from: {}", fxmlFile);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFile));
+        fxmlLoader.setControllerFactory(springContext::getBean);
+        rootNode = fxmlLoader.load();
+    }
+
+    @Override
     public void start(Stage stage) throws Exception {
 
         log.info("Starting Hello JavaFX and Maven demonstration application");
-
-        String fxmlFile = "/fxml/hello.fxml";
-        log.debug("Loading FXML for main view from: {}", fxmlFile);
-        FXMLLoader loader = new FXMLLoader();
-        Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
 
         log.debug("Showing JFX scene");
         Scene scene = new Scene(rootNode, 400, 200);
